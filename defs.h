275ad16023ca7ecc74d7facb1c2d727af7b41d3f
jmechnich <joerg.mechnich@gmail.com>
2016-12-22 15:49:46 +0100

    added fading, natural colors added defs.h

---
+++ defs.h
@@ -0,0 +1,81 @@
+#ifndef DEFS_H
+#define DEFS_H
+
+#define PIN        6
+#define NUMPIXELS 24
+
+#define PIN_RED   11
+#define PIN_GREEN  3
+#define PIN_BLUE   5
+
+#define RGBMAX   255
+#define RGBSTEP    3
+
+#define HSVMAX   1.0
+#define HSVSTEP  0.01
+
+#define DT_BLINK 150
+#define DT_CYCLE 200
+
+const uint8_t  cblack[3]   = {0};
+const uint8_t  cwhite[3]   = {RGBMAX,RGBMAX,RGBMAX};
+const uint8_t  cred[3]     = {RGBMAX,0,0};
+const uint8_t  cgreen[3]   = {0,RGBMAX,0};
+const uint8_t  cblue[3]    = {0,0,RGBMAX};
+const uint8_t  cyellow[3]  = {RGBMAX,RGBMAX,0};
+const uint8_t  cmagenta[3] = {RGBMAX,0,RGBMAX};
+const uint8_t  ccyan[3]    = {0,RGBMAX,RGBMAX};
+
+const uint8_t  csleep[3]   = {1,0,0};
+
+const uint8_t* crgb[]      = {cred,cgreen,cblue,0};
+const uint8_t* chsv[]      = {cyellow,cblack,cyellow,cblack,0};
+const uint8_t* cmax[]      = {cwhite,cblack,cwhite,cblack,0};
+
+const uint8_t  ccandle[3]     = {255, 147,  41}; // Candle, 1900K !
+const uint8_t  ctun40[3]      = {255, 197, 143}; // 40W Tungsten, 2600K
+const uint8_t  ctun100[3]     = {255, 214, 170}; // 100W Tungsten, 2850K
+const uint8_t  chalogen[3]    = {255, 241, 224}; // Halogen, 3200K
+const uint8_t  ccarbon[3]     = {255, 250, 244}; // Carbon Arc, 5200K
+const uint8_t  chighnoon[3]   = {255, 255, 251}; // High Noon Sun, 5400K
+const uint8_t  cdirectsun[3]  = {255, 255, 255}; // Direct Sunlight, 6000K
+const uint8_t  covercast[3]   = {201, 226, 255}; // Overcast Sky, 7000K
+const uint8_t  cclearsky[3]   = { 64, 156, 255}; // Clear Blue Sky, 20000K
+const uint8_t* cblackbody[] = {ccandle,ctun40,ctun100,chalogen,ccarbon,chighnoon,cdirectsun,covercast,cclearsky,0};
+
+const uint8_t  cfluorwarm[3]  = {255, 244, 229}; // Warm Fluorescent
+const uint8_t  cfluorstd[3]   = {244, 255, 250}; // Standard Fluorescent
+const uint8_t  cfluorcool[3]  = {212, 235, 255}; // Cool White Fluorescent
+const uint8_t  cfluorfull[3]  = {255, 244, 242}; // Full Spectrum Fluorescent
+const uint8_t  cfluorgrow[3]  = {255, 239, 247}; // Grow Light Fluorescent
+const uint8_t  cfluorblack[3] = {167,   0, 255}; // Black Light Fluorescent
+const uint8_t* cfluor[] = {cfluorwarm,cfluorstd,cfluorcool,cfluorfull,cfluorgrow,cfluorblack,0};
+
+const uint8_t  cmercvapor[3] = {216, 247, 255}; // Mercury Vapor
+const uint8_t  csodvapor[3]  = {255, 209, 178}; // Sodium Vapor !
+const uint8_t  chalide[3]    = {242, 252, 255}; // Metal Halide
+const uint8_t  csodhigh[3]   = {255, 183,  76}; // High Pressure Sodium !
+const uint8_t* cgas[] = {cmercvapor,csodvapor,chalide,csodhigh,0};
+
+enum GENMODES {
+    HSV,
+    RGB,
+    MAX,
+    GENMODES_SZ,
+};
+
+enum MODES {
+    RED=0,
+    YELLOW=0,
+    HUE=0,
+    GREEN=1,
+    MAGENTA=1,
+    SAT=1,
+    BLUE=2,
+    CYAN=2,
+    VAL=2,
+    ALL=3,
+    MODES_SZ,
+};
+
+#endif
--- ledlamp.ino
+++ ledlamp.ino
@@ -7,72 +7,91 @@
 #include <TimerOne.h>
 #include <RGBConverter.h>
 
-#define PIN        6
-#define NUMPIXELS 24
 
-#define PIN_RED   11
-#define PIN_GREEN  3
-#define PIN_BLUE   5
+#include "defs.h"
 
-#define RGBMAX   255
-#define RGBSTEP    3
+//const uint8_t** cpresets = cblackbody;
+//const uint8_t** cpresets = cfluor;
+//const uint8_t** cpresets = cgas;
+const uint8_t* cpresets[] = {ccandle,csodvapor,csodhigh,0};
 
-#define HSXMAX   1.0
-#define HSXSTEP  0.01
-
-#define DT_BLINK 150
-#define DT_CYCLE 200
-
-const uint8_t  cblack[3]   = {0};
-const uint8_t  cwhite[3]   = {RGBMAX,RGBMAX,RGBMAX};
-const uint8_t  cred[3]     = {RGBMAX,0,0};
-const uint8_t  cgreen[3]   = {0,RGBMAX,0};
-const uint8_t  cblue[3]    = {0,0,RGBMAX};
-const uint8_t  cyellow[3]  = {RGBMAX,RGBMAX,0};
-const uint8_t  cmagenta[3] = {RGBMAX,0,RGBMAX};
-const uint8_t  ccyan[3]    = {0,RGBMAX,RGBMAX};
-const uint8_t* crgb[]      = {cred,cgreen,cblue,0};
-const uint8_t* chsv[]      = {cyellow,cblack,cyellow,cblack,0};
-const uint8_t* chsl[]      = {ccyan,cblack,ccyan,cblack,0};
-const uint8_t* cmax[]      = {cwhite,cblack,cwhite,cblack,0};
-
+/*-------------------------------------------------------------------------
+ *  Variables
+ *-------------------------------------------------------------------------*/
 Adafruit_NeoPixel pixels =
     Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 ClickEncoder *encoder;
-int16_t last, value;
+int16_t last=-1, value=-1;
 bool longPress = false;
 bool isOn = false;
 
-double  hsxcolor[3] = {0.2,0.4,0.2};
 uint8_t rgbcolor[3] = {0};
-
-enum GENMODES {
-    HSV,
-    HSL,
-    RGB,
-    MAX,
-    GENMODES_SZ,
-};
-
-enum MODES {
-    RED=0,
-    YELLOW=0,
-    HUE=0,
-    GREEN=1,
-    MAGENTA=1,
-    SAT=1,
-    BLUE=2,
-    CYAN=2,
-    VAL=2,
-    LIGHT=2,
-    ALL=3,
-    MODES_SZ,
-};
-
-uint8_t general_mode = HSV;
+double  hsvcolor[3] = {0};
+uint8_t rgbsave[3] = {0};
+uint8_t brightness  = 255;
+    
+uint8_t curpreset = 0;
+uint8_t general_mode = RGB;
 uint8_t mode = general_mode == RGB ? ALL : VAL;
 
+const uint8_t fadetime   = 10;
+uint8_t fadensteps = 50;
+
+uint8_t fadecounter  = 0;
+uint8_t fadestep     = 0;
+bool    fading       = false;
+double  fadestart[3] = {0};
+uint8_t fadetarget[3]  = {0};
+
+void startFade(const uint8_t* rgb, const uint8_t* rgbcur=0)
+{
+  fading = true;
+  fadestep = 0;
+  fadecounter = 0;
+  memcpy(fadetarget,rgb,3);
+  if(!rgbcur) rgbcur = rgbcolor;
+
+  double max = 0;
+  for(uint8_t i=0;i<3;++i) {
+      fadestart[i] = rgbcur[i];
+      if(fabs(fadestart[i]-rgb[i]) > max)
+          max = fabs(fadestart[i]-rgb[i]);
+  }
+  //if(max > 50) max = 50;
+  fadensteps = uint8_t(max/5+1.5);
+}
+
+void fadeStep() 
+{
+  if(!fading) return;
+  if(++fadestep==fadensteps) {
+    fading = false;
+    memcpy(rgbcolor,fadetarget,3);
+    return;
+  }
+
+  Serial.print("fadestep ");
+  Serial.print(fadestep);
+  Serial.print(" color");
+  
+  for(uint8_t i=0;i<3;++i) {
+    double color = fadestart[i] + \
+        double(fadestep)/fadensteps*(fadetarget[i]-fadestart[i]);
+    if(color < 0)
+        color = 0;
+    else if(color > 255)
+        color = 255;
+    rgbcolor[i] = uint8_t(color+0.5);
+    Serial.print(" ");
+    Serial.print(rgbcolor[i]);
+  }
+  Serial.println();
+}
+  
+/*-------------------------------------------------------------------------
+ *  General helper functions
+ *-------------------------------------------------------------------------*/
 uint8_t colAverage() {
   int16_t avg = 0;
   for (uint8_t i=0; i<3; ++i)
@@ -85,10 +104,9 @@ uint8_t colAverage() {
   return avg;
 }
 
-void timerIsr() {
-  encoder->service();
-}
-
+/*-------------------------------------------------------------------------
+ *  Strip (WS2812 leds)
+ *-------------------------------------------------------------------------*/
 void setStrip( uint8_t r, uint8_t g, uint8_t b) {
   for(int i=0;i<NUMPIXELS;i++){
     pixels.setPixelColor(i, pixels.Color(r,g,b));
@@ -96,71 +114,77 @@ void setStrip( uint8_t r, uint8_t g, uint8_t b) {
   }
 }
 
-void setStrip( const uint8_t color[3]) {
-  setStrip( color[RED], color[GREEN], color[BLUE]);
+void setStrip( const uint8_t rgb[3]) {
+  setStrip( rgb[RED], rgb[GREEN], rgb[BLUE]);
 }
 
-void updateStrip() {
-  setStrip( rgbcolor);
+void setStrip() {
+  setStrip(rgbcolor);
 }
 
-void setLED( uint8_t r, uint8_t g, uint8_t b) {
-  analogWrite(PIN_RED,  255-r);
-  analogWrite(PIN_GREEN,255-g);
-  analogWrite(PIN_BLUE, 255-b);
+/*-------------------------------------------------------------------------
+ *  RGB LED of encoder
+ *-------------------------------------------------------------------------*/
+void setLED( uint8_t r, uint8_t g, uint8_t b, uint8_t bright) {
+  analogWrite(PIN_RED,  255-uint8_t(r/255.*bright+0.5));
+  analogWrite(PIN_GREEN,255-uint8_t(g/255.*bright+0.5));
+  analogWrite(PIN_BLUE, 255-uint8_t(b/255.*bright+0.5));
 }
 
-void setLED( const uint8_t color[3]) {
-  setLED( color[RED], color[GREEN], color[BLUE]);
+void setLED( const uint8_t rgb[3], uint8_t bright) {
+  setLED( rgb[RED], rgb[GREEN], rgb[BLUE], bright);
+}
+
+void setLED() 
+{
+  setLED(rgbcolor, brightness);
 }
 
 void blinkLED(bool invert=false) 
 {
   if(invert)
-      setLED(cwhite);
+      setLED(cwhite,255);
   else
-      setLED(cblack);
+      setLED(cblack,255);
   delay(DT_BLINK);
-  setLED(rgbcolor);
+  setLED();
 }
 
-void colorBlinkLED(const uint8_t color[3], uint8_t count=1) 
+void colorBlinkLED(const uint8_t rgb[3], uint8_t count=1) 
 {
   for( uint8_t i=0; i<count; ++i)
   {
-    setLED(color);
+    setLED(rgb,brightness);
     delay(DT_BLINK);
-    setLED(cblack);
+    setLED(cblack,brightness);
     delay(DT_BLINK);
   }
-  setLED(rgbcolor);
+  setLED();
 }
 
-void cycleLED( const uint8_t* colors[3])
+void cycleLED( const uint8_t* rgbs[3])
 {
   size_t i=0;
-  while(colors[i])
+  while(rgbs[i])
   {
-    setLED(colors[i]);
+    setLED(rgbs[i],brightness);
     delay(DT_CYCLE);
     ++i;
   }
-  setLED(rgbcolor);
+  setLED();
 }
 
-void updateLEDRGB() {
-  if( mode < ALL) {
-    uint8_t tmpcolor[3] = {0};
-    tmpcolor[mode] = rgbcolor[mode];
-    setLED( tmpcolor);
+/*-------------------------------------------------------------------------
+ *  Mode switching
+ *-------------------------------------------------------------------------*/
+void refresh()
+{
+  if(!isOn && (!fading)) {
+    setLED(csleep,255);
+  } else {
+    setLED();
   }
-  else {
-    setLED( rgbcolor);
-  }
-}
-
-void updateLEDHSX() {
-  setLED( rgbcolor);
+  setStrip();
 }
 
 void nextModeRGB() {
@@ -169,31 +193,97 @@ void nextModeRGB() {
   Serial.println(mode);
   uint8_t tmpcolor[3] = {0};
   if( mode < ALL) {
-    setLED( crgb[mode]);
+    setLED(crgb[mode],brightness);
     tmpcolor[mode] = rgbcolor[mode];
   }
   else {
-    setLED( cwhite);
+    setLED(cwhite,brightness);
     uint8_t avg = colAverage();
     for (uint8_t i=0; i<3; ++i) {
       rgbcolor[i] = avg;
       tmpcolor[i] = avg;
     }
-    updateStrip();
+    setStrip();
   }
   delay(DT_BLINK);
-  setLED(tmpcolor);
+  setLED(tmpcolor,brightness);
 }
 
-void nextModeHSX() {
+void nextModeHSV() {
   mode = (mode+1)%(MODES_SZ-1);
   Serial.print("Switching to mode ");
   Serial.println(mode);
-  if( general_mode == HSV)
-      colorBlinkLED(chsv[0],mode+1);
-  else
-      colorBlinkLED(chsl[0],mode+1);
-  setLED( rgbcolor);
+  colorBlinkLED(chsv[0],mode+1);
+  setLED();
+}
+
+void nextMode() {
+  switch(general_mode)
+  {
+  case HSV:
+    if( mode > VAL)
+        mode = VAL;
+    nextModeHSV();
+    break;
+  case RGB:
+    nextModeRGB();
+    break;
+  default:
+    break;
+  }
+}
+
+void nextGenMode()
+{
+  general_mode = (general_mode+1)%GENMODES_SZ;
+  switch( general_mode)
+  {
+  case HSV:
+    cycleLED(chsv);
+    mode = VAL;
+    refresh();
+    break;
+  case RGB:
+    cycleLED(crgb);
+    mode = RED;
+    refresh();
+    break;
+  case MAX:
+    cycleLED(cmax);
+    setLED(cwhite,brightness);
+    setStrip(cwhite);
+    break;
+  default:
+    break;
+  }
+}
+
+void nextPreset() 
+{
+  if(cpresets[++curpreset] == 0)
+      curpreset = 0;
+  startFade(cpresets[curpreset]);
+  Serial.print("Preset ");
+  Serial.println(curpreset);
+  
+  //setRGB(cpresets[curpreset]);
+  //refresh();
+}
+
+/*-------------------------------------------------------------------------
+ *  Color change
+ *-------------------------------------------------------------------------*/
+void setRGB(uint8_t r, uint8_t g, uint8_t b)
+{
+  rgbcolor[RED]   = r;
+  rgbcolor[GREEN] = g;
+  rgbcolor[BLUE]  = b;
+  RGBConverter::rgbToHsv(r, g, b, hsvcolor);
+}
+
+void setRGB(const uint8_t rgb[3])
+{
+  setRGB(rgb[RED],rgb[GREEN],rgb[BLUE]);
 }
 
 void decColRGB() {
@@ -212,22 +302,7 @@ void decColRGB() {
           rgbcolor[i] -= RGBSTEP;
     }
   }
-}
-
-void decColHSX() {
-  hsxcolor[mode] -= HSXSTEP;
-  if (hsxcolor[mode] < 0)
-  {
-    if( mode == HUE)
-        hsxcolor[mode] += 1;
-    else
-    {
-      blinkLED(mode==VAL);
-      hsxcolor[mode] = 0;
-    }
-  }
-  Serial.print("Setting value ");
-  Serial.println(hsxcolor[mode]);
+  RGBConverter::rgbToHsv(rgbcolor[RED], rgbcolor[GREEN], rgbcolor[BLUE], hsvcolor);
 }
 
 void incColRGB() {
@@ -251,91 +326,50 @@ void incColRGB() {
           rgbcolor[i] += RGBSTEP;
     }
   }
+  RGBConverter::rgbToHsv(rgbcolor[RED], rgbcolor[GREEN], rgbcolor[BLUE], hsvcolor);
 }
 
-void incColHSX() {
-  hsxcolor[mode] += HSXSTEP;
-  if (hsxcolor[mode] > 1)
+void setHSV(double h, double s, double v)
+{
+  hsvcolor[HUE] = h;
+  hsvcolor[SAT] = s;
+  hsvcolor[VAL] = v;
+  RGBConverter::hsvToRgb(h, s, v, rgbcolor);
+}
+
+void setHSV(double hsv[3])
+{
+  setHSV(hsv[HUE],hsv[SAT],hsv[VAL]);
+}
+
+void decColHSV() {
+  hsvcolor[mode] -= HSVSTEP;
+  if (hsvcolor[mode] < 0)
   {
     if( mode == HUE)
-        hsxcolor[mode] -= 1;
+        hsvcolor[mode] += 1;
+    else
+    {
+      blinkLED(mode==VAL);
+      hsvcolor[mode] = 0;
+    }
+  }
+  RGBConverter::hsvToRgb(hsvcolor[HUE], hsvcolor[SAT], hsvcolor[VAL], rgbcolor);
+}
+
+void incColHSV() {
+  hsvcolor[mode] += HSVSTEP;
+  if (hsvcolor[mode] > 1)
+  {
+    if( mode == HUE)
+        hsvcolor[mode] -= 1;
     else
     {
       blinkLED();
-      hsxcolor[mode] = 1;
+      hsvcolor[mode] = 1;
     }
   }
-  Serial.print("Setting value ");
-  Serial.println(hsxcolor[mode]);
-}
-
-void nextMode()
-{
-  switch(general_mode)
-  {
-  case HSV:
-  case HSL:
-    if( mode > VAL)
-        mode = VAL;
-    nextModeHSX();
-    break;
-  case RGB:
-    nextModeRGB();
-    break;
-  default:
-    break;
-  }
-}
-
-void nextGenMode()
-{
-  general_mode = (general_mode+1)%GENMODES_SZ;
-  switch( general_mode)
-  {
-  case HSV:
-    RGBConverter::rgbToHsv(
-        rgbcolor[RED], rgbcolor[GREEN], rgbcolor[BLUE], hsxcolor);
-    cycleLED(chsv);
-    mode = VAL;
-    display();
-    break;
-  case HSL:
-    RGBConverter::rgbToHsl(
-        rgbcolor[RED], rgbcolor[GREEN], rgbcolor[BLUE], hsxcolor);
-    cycleLED(chsl);
-    mode = LIGHT;
-    display();
-    break;
-  case RGB:
-    cycleLED(crgb);
-    mode = RED;
-    display();
-    break;
-  case MAX:
-    cycleLED(cmax);
-    isOn = true;
-    setLED(cwhite);
-    setStrip(cwhite);
-    break;
-  default:
-    break;
-  }
-}
-
-void updateLED()
-{
-  switch(general_mode)
-  {
-  case HSV:
-  case HSL:
-    updateLEDHSX();
-    break;
-  case RGB:
-    updateLEDRGB();
-    break;
-  default:
-    break;
-  }
+  RGBConverter::hsvToRgb(hsvcolor[HUE], hsvcolor[SAT], hsvcolor[VAL], rgbcolor);
 }
 
 void decCol()
@@ -343,8 +377,7 @@ void decCol()
   switch(general_mode)
   {
   case HSV:
-  case HSL:
-    decColHSX();
+    decColHSV();
     break;
   case RGB:
     decColRGB();
@@ -359,8 +392,7 @@ void incCol()
   switch(general_mode)
   {
   case HSV:
-  case HSL:
-    incColHSX();
+    incColHSV();
     break;
   case RGB:
     incColRGB();
@@ -370,54 +402,92 @@ void incCol()
   }
 }
 
-void display()
-{
-  switch(general_mode)
-  {
-  case HSV:
-    RGBConverter::hsvToRgb(
-        hsxcolor[HUE], hsxcolor[SAT], hsxcolor[VAL], rgbcolor);
-    break;
-  case HSL:
-    RGBConverter::hslToRgb2(
-        hsxcolor[HUE], hsxcolor[SAT], hsxcolor[LIGHT], rgbcolor);
-    break;
-  case MAX:
-    return;
-  default:
-    break;
-  }
-  updateLED();    
-  if( !isOn) return;
-  updateStrip();
+/*-------------------------------------------------------------------------
+ *  Encoder handlers
+ *-------------------------------------------------------------------------*/
+void timerIsr() {
+  encoder->service();
 }
 
-void setup() {
-  last = -1;
-  value= -1;
+bool toggleOn() {
+  isOn = !isOn;
+  if(isOn) {
+    startFade(rgbsave);
+  } else {
+    memcpy(rgbsave,rgbcolor,3);
+    startFade(cblack);
+  }
+}
 
+void handleLongPress() {
+  if(isOn) {
+    nextPreset();
+  }
+}
+
+void handleClick() {
+  toggleOn();
+}
+
+void handleDoubleClick() {
+  if(isOn) {
+  }
+}
+
+void handleTurnLeft() 
+{
+  if(!isOn) return;
+  if (brightness==0) {
+    blinkLED();
+    return;
+  }
+  brightness -= 5;
+  pixels.setBrightness(brightness);
+  refresh();
+}
+
+void handleTurnRight() 
+{
+  if(!isOn) return;
+  if (brightness==255) {
+    blinkLED();
+    return;
+  }
+  brightness += 5;
+  pixels.setBrightness(brightness);
+  refresh();
+}
+
+/*-------------------------------------------------------------------------
+ *  setup() and loop() functions
+ *-------------------------------------------------------------------------*/
+void setup() {
   Serial.begin(9600);
 
   pixels.begin();
+  pixels.setBrightness(brightness);
+  memcpy(rgbsave,cpresets[curpreset],3);
+  refresh();
 
   encoder = new ClickEncoder(A1, A0, A2, 4, LOW, HIGH);
   Timer1.initialize(1000);
   Timer1.attachInterrupt(timerIsr);
 
-  setStrip(cblack);
-  display();
 }
 
 void loop() {
+  if (fading && (++fadecounter%fadetime == 0))
+  {
+    fadecounter = 0;
+    fadeStep();
+    refresh();
+  }
   value += encoder->getValue();
-  
   if (value != last) {
     if (value < last)
-        decCol();
+        handleTurnLeft();
     else
-        incCol();
-    
-    display();
+        handleTurnRight();
     last = value;
   }
   
@@ -427,7 +497,7 @@ void loop() {
     case ClickEncoder::Held:
       if(!longPress)
       {
-        nextGenMode();
+        handleLongPress();
       }
       longPress = true;
       break;
@@ -435,16 +505,10 @@ void loop() {
       longPress = false;
       break;
     case ClickEncoder::Clicked:
-          nextMode();
+      handleClick();
       break;
     case ClickEncoder::DoubleClicked:
-        isOn = !isOn;
-        if( isOn)
-            display();
-        else
-        {
-          setStrip(cblack);
-        }
+      handleDoubleClick();
       break;
     default:
       break;
